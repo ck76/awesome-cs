dc5b1d3e58b502713dba089a2ba3d5264ec092c5
程坤酱 <chengkun0804@gmail.com>
2021-09-14 20:22:42 +0800

    我是shabi ，以后用github api

---
+++ BE/Python/Python基础/爬虫/github相关爬虫和脚本/githubAPI.py
@@ -0,0 +1,6 @@
+
+
+# https://gist.github.com/derhuerst/19e0844796fa3b62e1e9567a1dc0b5a3
+# https://github.community/t/how-can-i-know-the-number-of-repository-by-user-name/13767/3
+# curl -i -u ck76: https://api.github.com/users/ck76/starred
+# curl -i -u ck76: https://api.github.com/users/ck76/starred?per_page=20&page=20
---
+++ BE/Python/Python基础/爬虫/github相关爬虫和脚本/批量克隆github仓库到本地.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+disk_url = "/Users/chengkun02/Downloads/Github/"
+
+
+# 获取仓库信息
+
+
+
+# 批量克隆仓库
